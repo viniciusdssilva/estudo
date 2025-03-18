14fe61a18da37c6a644856dc4cbee9b8242f78f1
Vinicius dos Santos Silva <vinicius.silva@outlook.com>
2025-03-18 15:17:18 -0300

    Um domingo promissor

--- teste/teste.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd
-
-# Load JSON data
-with open('data.json') as file:
-    data = json.load(file)
-
-df = pd.DataFrame([data]) 
-df.to_excel('output.xlsx', index=False)
